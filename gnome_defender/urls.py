d43a04c08bf57535cfba67f79ad8d9602f1f0194
SirYancy <erickuha@gmail.com>
2016-02-19 17:45:24 -0600

    First Test

--- gnome_defender/urls.py
+++ gnome_defender/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'gnome_defender.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^tracker', include('tracker.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
