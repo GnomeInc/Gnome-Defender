0982cb7491693b3d72e957f23d5e355bb0f947be
SirYancy <erickuha@gmail.com>
2016-02-19 19:17:19 -0600

    Working on Models

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ This is the main repository for the open-source version of our backend software.
 
 ## Features
 
-- None yet
+### Models
+DataSet - Represents one data point
+- user: Reference to user ID
+- gnome: Reference to gnome which created this data point
+- date: date when this datum was harvested
+- time: time when this datum was harvested
+- temperature: temperature when datum was harvested
+
 
 ## How to Use
 
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class DataSet(models.Model):
+    user = models.ForeignKey('User', editable=False, on_delete=models.CASCADE)
+    gnome = models.ForeignKey('Gnome', editable=False, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    humidity = models.DecimalField(max_digits=5, decimal_places=2)
+    light_level = models.IntegerField()
+    soil_moisture = models.IntegerField()
+
+
+class Gnome(models.Model):
+    gnome_model = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    user = models.ForeignKey('User', editable=False, on_delete=models.CASCADE)
